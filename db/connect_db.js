d5b43d4d39f748b3e5a64c1590d15ad6c8561c86
NileshSingh123 <ns800622@gmail.com>
2023-05-24 09:02:52 -0700

    db changed

--- db/connect_db.js
+++ db/connect_db.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose')
-const url = "mongodb://127.0.0.1:27017/admissionportal"
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/admissionportal";
+const live_url =
+  "mongodb+srv://ns800622:neelesh123@cluster0.wpdlnci.mongodb.net/admissionportal?retryWrites=true&w=majority";
 
-const connectDB=()=>{
-    // For local DB
-    return mongoose.connect(url)
+const connectDB = () => {
+  // For local DB
+  return (
+    mongoose
+      .connect(live_url)
 
+      // For cloud DB
+      // return mongoose.connect(database)
 
-    // For cloud DB
-    // return mongoose.connect(database)
-    
-    .then(()=>{
-        console.log("Connected Succeessfully")
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
-}
-module.exports=connectDB
+      .then(() => {
+        console.log("Connected Succeessfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  );
+};
+module.exports = connectDB;
